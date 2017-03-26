eda28304bae708ff5205802b62a4d07b57d08c05
Daniel Jones <drjones18@gmail.com>
2017-03-26 09:20:18 -0400

    Improve stylesheet loading.

--- wp-content/themes/ppc-theme/functions.php
+++ wp-content/themes/ppc-theme/functions.php
@@ -5,10 +5,14 @@ function enqueue_jquery_tabs() {
 		wp_enqueue_script( 'toc-js', get_stylesheet_directory_uri() . '/js/ppc-toc.js', 'jquery');
 	}
 }
-
 add_action( 'wp_enqueue_scripts', 'enqueue_jquery_tabs');
 
 add_theme_support( "aesop-component-styles", array( "image", "quote", "gallery", "content", "video", "audio" ) );
+
+function ppc_enqueue_stylesheet() {
+	wp_enqueue_style( 'ppc', get_stylesheet_directory_uri() . '/style.css', array(), 1.0, 'all' );
+}
+add_action( 'wp_enqueue_scripts', 'ppc_enqueue_stylesheet' );
 
 function ppc_set_style_version( $styles ) {
 	$styles ->default_version = "20150605-2";
--- wp-content/themes/ppc-theme/header.php
+++ wp-content/themes/ppc-theme/header.php
@@ -5,9 +5,6 @@
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 	<title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
 	<meta name="viewport" content="width=device-width,initial-scale=1.0" />
-	<link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif:regular,bold" />
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
